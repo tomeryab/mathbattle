7d9a1763b348584ee2188ca464316c8b12a34df1
Tomer Yablonka <tomeryab@gmail.com>
2026-03-07 20:55:23 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
-import { TOPICS, generateQuestions } from "@/components/game/questionGenerator";
+import { TOPICS, generateQuestions } from "../components/game/questionGenerator";
 import { createPageUrl } from "@/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
